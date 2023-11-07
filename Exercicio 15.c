5c45da3ee7b468160ada83661297a70e8a74233f
nifry <144128828+nifry@users.noreply.github.com>
2023-11-07 11:41:07 -0300

    Add files via upload

---
+++ Exercicio 15.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+
+int main () {
+
+int opcao;
+    float valor; 
+    float resultado;
+
+    while (1) {
+        printf("\nEscolha uma opção de conversÃ£o:\n");
+        printf("1. Converter quilometros para milhas\n");
+        printf("2. Converter Celsius para Fahrenheit\n");
+        printf("0. Sair do programa\n");
+        printf("Opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 0:
+                printf("Saindo do programa...\n");
+                return 0;
+                case 1:
+                printf("Digite a distãncia em quilometros: ");
+                scanf("%f", &valor);
+                resultado = valor * 0.621371;
+                printf("%.2f quilometros é igual a %.2f milhas.\n", valor, resultado);
+                break;
+                case 2:
+                printf("Digite a temperatura em graus Celsius: ");
+                scanf("%f", &valor);
+                resultado = (valor * 9 / 5) + 32;
+                printf("%.2f graus Celsius Ã© igual a %.2f graus Fahrenheit.\n", valor, resultado);
+                break;
+                default:
+                printf("Opção invalida Escolha uma opção valida.\n");
+                break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
